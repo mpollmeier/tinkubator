fcf9e6b85ed3875d278842f38624d5f44901bd5b
Michael Pollmeier <michael@michaelpollmeier.com>
2013-01-06 13:08:18 +1300

    no need to override testCompliance (already called in super class)

--- gremlin-scala/src/test/scala/com/tinkerpop/gremlin/scala/filter/RangeStepTest.scala
+++ gremlin-scala/src/test/scala/com/tinkerpop/gremlin/scala/filter/RangeStepTest.scala
@@ -12,10 +12,6 @@ class RangeStepTest extends com.tinkerpop.gremlin.test.filter.RangeStepTest {
 
   val g = TinkerGraphFactory.createTinkerGraph();
 
-  override def testCompliance() {
-    ComplianceTest.testCompliance(this.getClass);
-  }
-
   def test_g_v1_out_rangeX0_1X() {
     super.test_g_v1_out_rangeX0_1X(g.v(1).out.range(0, 1));
   }
